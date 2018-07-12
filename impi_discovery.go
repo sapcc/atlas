ec450df54391dba7dafa9f6430dfa0f00dda2ad9
Stefan Hipfel <stefan.hipfel@googlemail.com>
2018-07-12 17:26:13 +0200

    Create impi_discovery.go

---
+++ impi_discovery.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/go-kit/kit/log"
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/openstack"
+	"github.com/gophercloud/gophercloud/openstack/identity/v3/tokens"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/discovery/targetgroup"
+	"github.com/prometheus/prometheus/documentation/examples/custom-sd/adapter"
+)
+
+var (
+	outputFile = flag.String("output.file", "impi_sd.json", "Output file for file_sd compatible file.")
+	logger     log.Logger
+)
+
+type discovery struct {
+	ironicClient     ironicClient
+	address          string
+	refreshInterval  int
+	clientDatacenter string
+	tagSeparator     string
+	logger           log.Logger
+}
+
+func (d *discovery) parseServiceNodes() (*targetgroup.Group, error) {
+	tgroup := targetgroup.Group{
+		Source: "ipmi",
+		Labels: make(model.LabelSet),
+	}
+
+	nodes, err := d.ironicClient.GetNodes()
+
+	if err != nil {
+		logger.Log(err.Error())
+		return nil, err
+	}
+	tgroup.Targets = make([]model.LabelSet, 0, len(nodes))
+
+	for _, node := range nodes {
+		target := model.LabelSet{model.AddressLabel: model.LabelValue(node.DriverInfo.IpmiAddress)}
+		labels := model.LabelSet{
+			model.LabelName("job"): "impi",
+		}
+		tgroup.Labels = labels
+
+		tgroup.Targets = append(tgroup.Targets, target)
+	}
+
+	return &tgroup, nil
+}
+
+// Note: create a config struct for your custom SD type here.
+type sdConfig struct {
+	IronicClient    *ironicClient
+	RefreshInterval int
+}
+
+func newDiscovery(conf sdConfig) (*discovery, error) {
+	cd := &discovery{
+		ironicClient:    *conf.IronicClient,
+		refreshInterval: conf.RefreshInterval,
+		logger:          logger,
+	}
+	return cd, nil
+}
+
+func (d *discovery) Run(ctx context.Context, ch chan<- []*targetgroup.Group) {
+	for c := time.Tick(time.Duration(d.refreshInterval) * time.Second); ; {
+		var tgs []*targetgroup.Group
+		tg, err := d.parseServiceNodes()
+		tgs = append(tgs, tg)
+		if err == nil {
+			ch <- tgs
+		}
+		// Wait for ticker or exit when ctx is closed.
+		select {
+		case <-c:
+			continue
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func main() {
+
+	authOptions := &tokens.AuthOptions{
+		IdentityEndpoint: "https",
+		Username:         "",
+		Password:         "",
+		DomainName:       "",
+		AllowReauth:      true,
+		Scope: tokens.Scope{
+			ProjectName: "",
+			DomainName:  "",
+		},
+	}
+
+	provider, err := openstack.NewClient("https")
+	if err != nil {
+		fmt.Errorf("could not initialize openstack client: %v", err)
+	}
+
+	err = openstack.AuthenticateV3(provider, authOptions, gophercloud.EndpointOpts{})
+	if err != nil {
+		fmt.Errorf("could not authenticat provider client: %v", err)
+	}
+
+	ic, err := NewIronicClient(provider)
+
+	ctx := context.Background()
+
+	// NOTE: create an instance of your new SD implementation here.
+	cfg := sdConfig{
+		RefreshInterval: 30,
+		IronicClient:    ic,
+	}
+
+	disc, err := newDiscovery(cfg)
+	if err != nil {
+		fmt.Println("err: ", err)
+	}
+	sdAdapter := adapter.NewAdapter(ctx, *outputFile, "impiDiscovery", disc, logger)
+	sdAdapter.Run()
+
+	<-ctx.Done()
+}
